ea5fb26b5686c21e43b5ba08cba137827650cbd5
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-12-22 18:40:05 +0530

    update backend URL in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",   // backend URL
+  baseURL: "https://equalsplit-backend-89.onrender.com",   // backend URL
 });
 
 api.interceptors.request.use((config) => {
