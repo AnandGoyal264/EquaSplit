6d0634401af858d71141160bfc551d9bb800065f
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-12-24 02:35:53 +0530

    returnadded

--- src/api/expense.api.js
+++ src/api/expense.api.js
@@ -2,5 +2,5 @@ import api from "./axios";
 
 /* groupId MUST be passed as argument */
 export const getGroupExpenses = (groupId) => {
-  api.get(`/expenses/groups/${groupId}`);
+  return api.get(`/expenses/groups/${groupId}`);
 };
